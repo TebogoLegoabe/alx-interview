ada9e2c5c3c717d968a31ce69b85e4160c5d6111
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-12-08 01:15:44 +0200

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 """lockboses"""
 
-def canUnlockAll(boxes):
-    """method that determines if all the boxes can be opened"""
 
+def canUnlockAll(boxes):
+    # method that determines if all the boxes can be opened
+    if not boxes:
+        return False
+    # initialize variables for stack and list
+    opened_boxes = set()
+    opened_boxes.add(0)
+    keys_stack = list(boxes[0])
+
+    while keys_stack:
+        key = keys_stack.pop()
+        if key < len(boxes) and key not in opened_boxes:
+            opened_boxes.add(key)
+            keys_stack.extend(boxes[key])
+
+    return len(opened_boxes) == len(boxes)
