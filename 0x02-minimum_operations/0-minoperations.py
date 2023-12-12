a6254ee4f859e6c0a58e9894d6fc4a42bb2c1d32
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-12-12 15:38:27 +0200

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
 """
-method that calculates the fewest number of operations needed
-to result in exactly n H characters in the file
+Text file with character H,can only execute two operations in the file:
+copy all and paste
 """
 
 
-def minOperations(n):
+def  minOperations(n):
     """
-    
-    """
+    method that calculates the fewest numbers of operations needed to result
+    in exactly n H characters in the file.
 
+    Return: 
+    integer
+    if n is impossible to achieve, return 0
+    """
+    if n == 1 or n < 0:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 2:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+    return operations
